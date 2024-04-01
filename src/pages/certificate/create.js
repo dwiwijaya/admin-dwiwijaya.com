38e988e67dc94c2a6f1e52aa192847b578ffe926
Dwi Wijaya <dev.dwiwijaya@gmail.com>
2024-04-02 00:36:51 +0700

    dummy iniitial pages skill, portfolio and certificate

---
+++ src/pages/certificate/create.js
@@ -0,0 +1,17 @@
+import { BackButton } from "@/components/common/BackButton";
+import PageHeading from "@/components/common/PageHeading";
+import Container from "@/components/layout/Container";
+import CreateBlog from "@/components/views/blog/create";
+import Link from "next/link";
+import React from "react";
+
+const create = () => {
+  return (
+    <Container>
+      <PageHeading title="Create Certificate" />
+      <CreateBlog />
+    </Container>
+  );
+};
+
+export default create;
---
+++ src/pages/skill/create.js
@@ -0,0 +1,16 @@
+import PageHeading from '@/components/common/PageHeading'
+import SkillForm from '@/components/form/SkillForm'
+import Container from '@/components/layout/Container'
+import Link from 'next/link'
+import React from 'react'
+
+const create = () => {
+  return (
+    <Container>
+      <PageHeading title="Create Skill">
+      </PageHeading>
+    </Container>
+  )
+}
+
+export default create
