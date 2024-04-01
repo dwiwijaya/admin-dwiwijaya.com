6afdb076f54f58e7272ed52af439250a1040ea10
Dwi Wijaya <dev.dwiwijaya@gmail.com>
2024-04-02 00:35:51 +0700

    fix: page dashboard wrapp by container

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -1,11 +1,12 @@
+import Container from '@/components/layout/Container'
 import Dashboard from '@/components/views/dashboard/Dashboard'
 import React from 'react'
 
 const dashboard = () => {
   return (
-    <>
+    <Container>
         <Dashboard/>
-    </>
+    </Container>
   )
 }
 
