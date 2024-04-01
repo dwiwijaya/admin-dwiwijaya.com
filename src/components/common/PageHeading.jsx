1ea374b917278a5f0d5556aae463a03c880efa54
Dwi Wjiaya <dev.dwiwijaya@gmail.com>
2024-04-01 13:18:11 +0700

    add: pageheading component children

--- src/components/common/PageHeading.jsx
+++ src/components/common/PageHeading.jsx
@@ -1,9 +1,13 @@
 import React from "react";  
 
-  const PageHeading = ({ title, description }) => {
+  const PageHeading = ({ title, description, children }) => {
     return (
       <>
+      <div className="flex items-center justify-between">
         <h2 className="ml-3 relative font-bold text-3xl text-text dots">{title}</h2>
+        {children}
+
+      </div>
         <p className="leading-5 text-subtext">
           {description}
         </p>
