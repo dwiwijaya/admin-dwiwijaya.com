dddb58acb0180326273683a45b75508c23fb92fe
Dwi Wjiaya <dev.dwiwijaya@gmail.com>
2024-04-01 13:15:41 +0700

    fix: theme toggle add html attribute color-mode

--- src/components/toggles/ThemeToggle.jsx
+++ src/components/toggles/ThemeToggle.jsx
@@ -1,21 +1,26 @@
-// ThemeToggle.js
-
 import { useTheme } from "next-themes";
 import { useEffect, useState } from "react";
 
 const ThemeToggle = () => {
-    const { theme, setTheme } = useTheme( );
-    const [mounted, setMounted] = useState(false) 
-    const toggleTheme = () =>
+    const {theme, setTheme} = useTheme();
+    const [mounted, setMounted] = useState(false);
 
-    setTheme(theme === 'light' ? 'dark' : 'light');
+    const toggleTheme = () => {
+        const newTheme = theme === "light" ? "dark" : "light";
+        document.documentElement.setAttribute("data-color-mode", newTheme);
+        setTheme(newTheme);
+    };
+
     useEffect(() => {
-        setMounted(true)
-    }, [])
+        document.documentElement.setAttribute("data-color-mode", theme);
+
+        setMounted(true);
+    }, []);
 
     if (!mounted) {
-        return null
+        return null;
     }
+
     return (
         <button className='group  active:ring toggle right-5 theme__toggle' onClick={toggleTheme}>
             <i className={`group-active:rotate-90 transition-3s bx bx-${theme == 'dark' ? 'moon' : 'sun'}`}  />
